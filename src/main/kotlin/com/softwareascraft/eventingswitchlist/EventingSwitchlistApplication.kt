f0b4ce3f2731c5d8f5089d978768a817e2801de8
Paige Watson <paige.watson@outlook.com>
2024-04-13 19:18:21 -0700

    initial commit

---
+++ src/main/kotlin/com/softwareascraft/eventingswitchlist/EventingSwitchlistApplication.kt
@@ -0,0 +1,11 @@
+package com.softwareascraft.eventingswitchlist
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class EventingSwitchlistApplication
+
+fun main(args: Array<String>) {
+    runApplication<EventingSwitchlistApplication>(*args)
+}
---
+++ src/test/kotlin/com/softwareascraft/eventingswitchlist/EventingSwitchlistApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.softwareascraft.eventingswitchlist
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class EventingSwitchlistApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
