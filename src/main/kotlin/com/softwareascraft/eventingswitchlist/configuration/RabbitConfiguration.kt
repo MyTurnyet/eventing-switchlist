75ed8b996444051cbce0c5de2b5b68c262954b9c
Paige Watson <paige.watson@outlook.com>
2024-04-14 10:13:33 -0700

    test queue name

--- src/main/kotlin/com/softwareascraft/eventingswitchlist/configuration/RabbitConfiguration.kt
+++ src/main/kotlin/com/softwareascraft/eventingswitchlist/configuration/RabbitConfiguration.kt
@@ -1,5 +1,14 @@
 package com.softwareascraft.eventingswitchlist.configuration
 
+import org.springframework.amqp.core.Queue
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+
+@Configuration
 class RabbitConfiguration {
+   @Bean
+    fun queue():Queue {
+        return Queue("eventing-switchlist")
+    }
 
 }
--- src/test/kotlin/com/softwareascraft/eventingswitchlist/configuration/RabbitConfigurationTests.kt
+++ src/test/kotlin/com/softwareascraft/eventingswitchlist/configuration/RabbitConfigurationTests.kt
@@ -1,15 +1,18 @@
 package com.softwareascraft.eventingswitchlist.configuration
 
 import assertk.assertThat
-import assertk.assertions.isInstanceOf
-import assertk.assertions.isNotNull
+import assertk.assertions.isEqualTo
 import org.junit.jupiter.api.Tag
 import org.junit.jupiter.api.Test
+import org.springframework.amqp.core.Queue
 
 @Tag("unit")
 class RabbitConfigurationTests {
     @Test
     fun `creates RabbitConfiguration`() {
         val obj = RabbitConfiguration()
+        val queue: Queue = obj.queue()
+        assertThat(queue.name).isEqualTo("eventing-switchlist")
+
     }
 }
