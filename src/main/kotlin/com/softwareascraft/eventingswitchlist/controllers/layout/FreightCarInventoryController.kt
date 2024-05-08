1919f24e26a88ca3c40fd66645c7a840ebc448e5
paige.watson <paige.watson@salesforce.com>
2024-05-08 12:34:49 -0700

    add Mapping for endpoints

--- src/main/kotlin/com/softwareascraft/eventingswitchlist/controllers/layout/FreightCarInventoryController.kt
+++ src/main/kotlin/com/softwareascraft/eventingswitchlist/controllers/layout/FreightCarInventoryController.kt
@@ -3,13 +3,20 @@ package com.softwareascraft.eventingswitchlist.controllers.layout
 import com.softwareascraft.eventingswitchlist.data.FreightCarRepository
 import com.softwareascraft.eventingswitchlist.models.FreightCar
 import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PathVariable
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
 
+@RestController
+@RequestMapping("/api/freight-cars")
 class FreightCarInventoryController(val repository: FreightCarRepository) {
-    fun getCarById(freightCarId: String): ResponseEntity<FreightCar> {
+    @GetMapping("/{freightCarId}")
+    fun getCarById(@PathVariable freightCarId: String): ResponseEntity<FreightCar> {
         val freightCar = repository.findById(freightCarId)
         return ResponseEntity.ok(freightCar)
     }
-
+    @GetMapping("/")
     fun getAll(): ResponseEntity<List<FreightCar>> {
         val freightCarList = repository.getAll()
         return ResponseEntity.ok(freightCarList)
--- src/test/kotlin/com/softwareascraft/eventingswitchlist/controllers/layout/FreightCarInventroyTests.kt
+++ src/test/kotlin/com/softwareascraft/eventingswitchlist/controllers/layout/FreightCarInventroyTests.kt
@@ -3,12 +3,19 @@ package com.softwareascraft.eventingswitchlist.controllers.layout
 import assertk.assertThat
 import assertk.assertions.isEqualTo
 import assertk.assertions.isNotEmpty
+import com.softwareascraft.eventingswitchlist.controllers.status.CarStatusControllerTestConfiguration
 import com.softwareascraft.eventingswitchlist.data.FakeFreightCarRepository
 import com.softwareascraft.eventingswitchlist.models.FreightCar
 import com.softwareascraft.eventingswitchlist.models.RollingStockDto
+import org.assertj.core.api.Assertions
 import org.bson.types.ObjectId
 import org.junit.jupiter.api.Tag
 import org.junit.jupiter.api.Test
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.boot.test.web.client.TestRestTemplate
+import org.springframework.boot.test.web.client.getForEntity
+import org.springframework.context.annotation.Import
 import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
 
@@ -44,6 +51,18 @@ class FreightCarInventoryControllerTests {
         val freightCars = responseEntity.body!!
         assertThat(freightCars).isNotEmpty()
         assertThat(freightCars.size).isEqualTo(2)
+
+    }
+}
+
+@Tag("integration")
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@Import(CarStatusControllerTestConfiguration::class)
+class FreightCarIntegrationTests(@Autowired val restTemplate: TestRestTemplate) {
+    @Test
+    fun `calls add pizza order on controller`() {
+        val entity = restTemplate.getForEntity<FreightCar>("/api/freight-cars")
+        Assertions.assertThat(entity.statusCode).isEqualTo(HttpStatus.OK)
 
     }
 }
